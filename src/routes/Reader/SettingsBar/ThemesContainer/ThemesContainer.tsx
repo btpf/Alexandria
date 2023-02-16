bdecce0bdc277c10701e869f20dae44b6d603893
btpf <bretpapkoff@Gmail.com>
2023-02-16 17:08:17 -0500

    Connected appThemes With ThemeContainer

--- src/routes/Reader/SettingsBar/ThemesContainer/ThemesContainer.tsx
+++ src/routes/Reader/SettingsBar/ThemesContainer/ThemesContainer.tsx
@@ -3,62 +3,26 @@ import React, { useEffect, useState } from 'react'
 import styles from './ThemesContainer.module.scss'
 
 import { useAppDispatch, useAppSelector } from '@store/hooks'
-import { SetTheme, Theme } from '@store/slices/bookState'
-
-const dark = {
-  body: {
-    background: `#444`,
-    color: `#fff`,
-  },
-  'a:link': {
-    color: `#1e83d2`,
-    'text-decoration': 'none',
-  },
-  'a:link:hover': {
-    background: 'rgba(0, 0, 0, 0.1)',
-  },
-};
-
-const Base:Theme = {
-  body: {
-    background: `white`,
-    color: `black`,
-  },
-
-  'a:link': {
-    color: `#0000EE`,
-    'text-decoration': 'inherit',
-  },
-  'a:link:hover': {
-    background: 'inherit',
-  },
-};
-
+import { SetTheme } from '@store/slices/bookState'
+import { Theme } from '@store/slices/EpubJSBackend/data/theme/themeManager.d';
 
 interface ThemeInterface{
   [name: string]: Theme
 }
-const testThemes: ThemeInterface = {
-  Dark: dark, 
-//   Default: Base
-}
+
 
 const ThemesContainer = ()=>{
   const dispatch = useAppDispatch()
+  const appThemes = useAppSelector((state) => state.app.themes)
   const fontSize = useAppSelector((state) => state.bookState[0]?.data.theme.fontSize)
   return ( 
     
     <div className={styles.themeSelectorContainer}>
-      <div onClick={()=>{
-        dispatch(SetTheme({view:0, theme:Base}))}}
-      style={{backgroundColor: 'white', color:'black'}} className={styles.theme}>
-          Default
-      </div>
-      {Object.keys(testThemes).map((item)=>{
-        const {background, color} = (testThemes[item]).body
+      {Object.keys(appThemes).map((item)=>{
+        const {background, color} = (appThemes[item]).body
         return (
           <div key={item} onClick={()=>{
-            dispatch(SetTheme({view:0, theme:testThemes[item]}))
+            dispatch(SetTheme({view:0, theme:appThemes[item]}))
           }} style={{backgroundColor: background, color}} className={styles.theme}>
             {item}
           </div>
