29270db68b1707c78654d8535630c22457c4663e
btpf <bretpapkoff@Gmail.com>
2023-07-24 17:46:54 -0400

    Fixed Homepage Icon Stroke Width & User Events On Title

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -213,9 +213,9 @@ const Shelf = () =>{
 
         </div>
         <div className={styles.rightContainer}>
-          <Filter style={{marginRight:10, strokeWidth: 1}} onClick={()=>setBottomBarActive(!bottomBarActive)}/>
+          <Filter style={{marginRight:10, strokeWidth: 1.5, cursor:"pointer"}} onClick={()=>setBottomBarActive(!bottomBarActive)}/>
           <Link className={styles.unstyleLink} to="/settings">
-            <Settings style={{strokeWidth: 1}}/>
+            <Settings style={{strokeWidth: 1.5}}/>
           </Link>
           <TitleBarButtons/>
         </div>
